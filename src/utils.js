bd430aed9bfbd43700f43eb659b080d14c7291d5
laurafolk <laurafolk@yahoo.com>
2025-04-17 15:03:19 -0500

    Working on updating errors.

--- src/utils.js
+++ src/utils.js
@@ -9,4 +9,13 @@ export function $(selector) {
   export function getFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key)) || null;
   }
-  
+
+  export function renderFavorites() {
+    document.getElementById("app").innerHTML = `
+      <section class="home">
+        <h1>These are your favorites</h1>
+        <p>Coming soon...</p>
+        <a href="#/search" class="btn">Search Recipes</a>
+      </section>
+    `;
+  }
