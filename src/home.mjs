4029558d326795b91f67c9d1c5b9bfb51e7cbcf4
laurafolk <laurafolk@yahoo.com>
2025-04-17 17:24:09 -0500

    Worked on getting final project running.

--- src/home.mjs
+++ src/home.mjs
@@ -2,7 +2,8 @@ export function renderHome() {
     document.getElementById("app").innerHTML = `
       <section class="home">
         <h1>Welcome to The Complete Recipe Meal Prep Planner</h1>
-        <p>Designed for busy individuals who want to eat well without stress.</p>
+        <p>Designed for busy individuals who want to eat well without stress, help make plan meals effortlessly. You can search recipes, save your favorites and help make your weekly meal prep 
+          a little easier</p>
         <a href="#/search" class="btn">Search Recipes</a>
       </section>
     `;
